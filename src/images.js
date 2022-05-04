89b24f13e5d2185b5da037b2f68e87da825b110a
GamsengMazee <gamsengmazeemarak@gmail.com>
2022-05-04 15:27:57 +0530

    first commit

---
+++ src/images.js
@@ -0,0 +1,10 @@
+import image1 from './images/img1.jpg'
+import image2 from './images/img2.jpg'
+import image3 from './images/img3.jpg'
+import image4 from './images/img4.jpg'
+import image5 from './images/img5.jpg'
+import image6 from './images/img6.jpg'
+
+const images =  [image1, image2, image3, image4, image5, image6]
+
+export default images
